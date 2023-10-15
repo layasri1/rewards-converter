bcad103fa454b95379beae68f6e14b4d6ab9aaa0
layasri1 <layasri1250@gmail.com>
2023-10-15 19:26:03 +0530

    Added code

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,30 @@
 public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
+
+    // Constructor that accepts a value in miles
+    public RewardValue(double milesValue, boolean isMiles) {
+        if (isMiles) {
+            this.milesValue = milesValue;
+            this.cashValue = milesValue * 0.0035;
+        } else {
+            // Handle error or default behavior here if needed
+        }
+    }
+
+    // Get the cash value of the RewardValue
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Get how many miles the RewardValue is worth
+    public double getMilesValue() {
+        return milesValue;
+    }
 }
